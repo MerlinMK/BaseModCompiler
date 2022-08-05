6015ef511520c40faf44e6dd2801bb162c738cf5
MerlinMK <110626564+MerlinMK@users.noreply.github.com>
2022-08-05 06:25:46 +0200

    Delete lua directory
    Signed-off-by: MerlinMK <110626564+MerlinMK@users.noreply.github.com>

--- lua/client/MyModName/ServerCommands/Pong.lua
+++
@@ -1,8 +0,0 @@
---- Load Client
-local Client = require 'MyModName/Client';
-
----Handle receiving Pong command from the server
----@param args table
-function Client.Commands.Pong(args)
-    Client.Log("Received Pong!");
-end
